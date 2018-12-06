2ad1a0b02af72b8c7a5b217234d86226880bfe87
David Orriss <dorriss@expedia.com>
2018-12-06 10:18:09 -0800

    lots of updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,10 @@
 module.exports = {
-  entry: './public/app.jsx',
+  entry: './public/app.js',
   output: {
     path: __dirname,
     filename: './public/bundle.js'
   },
   resolve: {
-    root: __dirname,
-    alias: {
-      Greeter: 'public/components/Greeter.jsx',
-      GreeterMessage: 'public/components/GreeterMessage.jsx',
-      GreeterForm: 'public/components/GreeterForm.jsx'
-    },
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [
-      {
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-        },
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/
-      }
-    ]
+    extensions: ['*', '.js', '.jsx']
   }
 };
